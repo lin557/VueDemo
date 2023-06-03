88218d7cd4bc507875ddd89459ef752648eec728
lin557 <lin557@126.com>
2023-06-03 15:41:16 +0800

    ✨ feat: QQ音乐filter效果

---
+++ v3-qq-music/src/App.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { ref } from 'vue' 
+import QqMusic from './components/qq-music.vue'
+import bg01 from './assets/bg01.webp'
+import bg02 from './assets/bg02.webp'
+import bg03 from './assets/bg03.webp'
+
+const imgUrl = ref('')
+
+const bgColor = ref('rgb(167, 137, 113)')
+
+imgUrl.value = bg01
+</script>
+
+<template>
+  <div class="test">
+    <button @click="imgUrl=bg01;bgColor='rgb(70, 88, 94)'">bg01</button>
+    <button @click="imgUrl=bg02;bgColor='rgb(167, 137, 113)'">bg02</button>
+    <button @click="imgUrl=bg03;bgColor='rgb(196, 196, 142)'">bg03</button>
+  </div>
+
+  <QqMusic :poster='imgUrl' :color="bgColor" />
+</template>
+
+<style>
+.test {
+  position: absolute;
+  width: 100%;
+  margin-top: 20px;
+  z-index: 100;
+}
+
+.test > button {
+  opacity: .8;
+  border-color: hsla(0,0%,100%,.2);
+  background-color: transparent;
+  color: #fff;
+  min-width: 122px;
+  text-align: center;
+
+  border-radius: 2px;
+  font-size: 14px;
+  margin-right: 6px;
+  padding: 0 23px;
+  height: 38px;
+  line-height: 38px;
+  display: inline-block;
+  white-space: nowrap;
+  box-sizing: border-box;
+  overflow: hidden;
+}
+
+.test > button:hover {
+  opacity: 1;
+  filter: none;
+  border-color: #fff;
+}
+</style>
---
+++ v3-qq-music/src/components/qq-music.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const bgStyle = computed(() => {
+  return 'background-image: url(' + props.poster + ');'
+})
+
+const bgColor = computed(() => {
+  return 'background-color: ' + props.color + ';'
+})
+
+const props = defineProps<{ poster: string, color: string }>()
+
+</script>
+
+<template>
+  <div class="player" :style="bgColor">
+    <h1 class="player-logo">QQ音乐</h1>
+    <div class="player-bg-mask"></div>
+    <div class="player-bg" :style="bgStyle"></div>
+  </div>
+</template>
+
+<style scoped>
+.player {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+.player-logo {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  z-index: 3;
+}
+.player-bg-mask {
+  background-color: rgba(0,0,0,.35);
+  z-index: 2;
+}
+
+.player-bg, .player-bg-mask {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  top: 0;
+  left: 0;
+  position: absolute;
+}
+
+.player-bg {
+  display: block;
+  background-color: #fff;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 50%;
+  filter: blur(65px);
+  opacity: .6;
+  transform: translateZ(0);
+}
+</style>
